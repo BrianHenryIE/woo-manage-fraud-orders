6d713bde3ac090d92504cec4003a35c753794476
Brian Henry <BrianHenryIE@gmail.com>
2021-07-12 23:27:41 -0700

    Move plugins page settings link to own class

---
+++ src/Admin/class-plugins-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Add the `Settings` link on plugins.php.
+ *
+ * @package woo-manage-fraud-orders
+ */
+
+/**
+ * Class Plugins_Page
+ */
+class Plugins_Page {
+
+	/**
+	 * Add the `Settings` link under the plugin name on plugins.php.
+	 *
+	 * @hooked plugin_action_links_{plugin_basename}
+	 * @see WP_Plugins_List_Table::single_row()
+	 *
+	 * @param array<string, string> $actions The existing registered links.
+	 * @return array<string, string>
+	 */
+	public static function action_links( $actions ): array {
+
+		$new_actions = array(
+			'settings' => '<a href="' . admin_url( 'admin.php?page=wc-settings&tab=settings_tab_blacklists' ) . '">' . __( 'Settings', 'woo-manage-fraud-orders' ) . '</a>',
+		);
+
+		return array_merge( $new_actions, $actions );
+	}
+
+}
---
+++ src/autoload.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . '/Admin/class-plugins-page.php';
